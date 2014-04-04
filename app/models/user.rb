45d655f4bf0c0d5cf475c238094f119666820bfc
ivannasya <vagrant@vagrant.(none)>
2014-04-04 10:00:22 +0000

    mandrill

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   symbolize :gender, in: [:m, :f], allow_blank: true, allow_nil: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325082102) do
+ActiveRecord::Schema.define(version: 20140401104323) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -31,6 +34,10 @@ ActiveRecord::Schema.define(version: 20140325082102) do
     t.string   "gender"
     t.boolean  "admin",                  default: false, null: false
     t.string   "city"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
