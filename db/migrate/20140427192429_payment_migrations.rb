a90e428509075e6b7b46309ee56fc6c9cea0dd7a
ivannasya <iva.anastya@gmail.com>
2014-05-09 03:32:15 +0400

    Delete 20140427192429_payment_migrations.rb

--- db/migrate/20140427192429_payment_migrations.rb
+++
@@ -1,9 +0,0 @@
-class PaymentMigrations < ActiveRecord::Migration
-  def change
-    add_column :vendors, :commission_yandex, :float
-    add_column :vendors, :commission_web_money, :float
-
-    add_column :transactions, :payment_type, :integer, default: 1
-    add_column :transactions, :order_id, :bigint
-  end
-end
