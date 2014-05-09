7d175e63e56538c5aaa4da342e9ef54cf80a86a0
Nikita Mamonov <mamonov.nick@gmail.com>
2014-05-09 04:42:54 +0400

    lolz

--- app/controllers/update_table_controller.rb
+++ app/controllers/update_table_controller.rb
@@ -1,5 +1,7 @@
 #encoding: utf-8
 class UpdateTableController < ApplicationController
+  skip_before_filter :require_current_user
+  
   def  update_db
     #UpdateDbWorker.perform_async
     UpdateTable.service_types
