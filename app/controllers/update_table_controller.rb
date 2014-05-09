3c1ef2db05584e04afd4089e20091d1530fe96ba
Nikita Mamonov <mamonov.nick@gmail.com>
2014-05-09 05:09:25 +0400

    lolz

--- app/controllers/update_table_controller.rb
+++ app/controllers/update_table_controller.rb
@@ -3,13 +3,13 @@ class UpdateTableController < ApplicationController
   skip_before_filter :require_current_user
 
   def  update_db
-    UpdateDbWorker.perform_async
-    # UpdateTable.service_types
-    # UpdateTable.vendors
-    # UpdateTable.places
-    # UpdateTable.services
-    # UpdateTable.tariff_templates
-    # UpdateTable.users
+    #UpdateDbWorker.perform_async
+    UpdateTable.service_types
+    UpdateTable.vendors
+    UpdateTable.places
+    UpdateTable.services
+    UpdateTable.tariff_templates
+    UpdateTable.users
     render json: true
   end
 end
