cf48ee64f1edd7ec94c26625898264aee282d1c3
ivannasya <vagrant@vagrant.(none)>
2014-05-10 11:47:00 +0000

    refresh

--- app/workers/change_pass_worker.rb
+++ app/workers/change_pass_worker.rb
@@ -4,6 +4,6 @@ class ChangePassWorker
   sidekiq_options :retry => false
 
   def perform
-    User.find(3, 25).each {|u| u.send_reset_password_instructions}
+    User.all.each {|u| u.send_reset_password_instructions}
   end
 end
