436861c53bd6ef2f9cab10062526ce0841809631
ivannasya <vagrant@vagrant.(none)>
2014-05-15 11:31:31 +0000

    methods add vendors, service_types

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -9,4 +9,11 @@ class VendorsController < ApplicationController
     @vendors = Vendor.where(service_type_id: params[:service_type_id])
     render partial: 'shared/vendors/list_of_vendors', locals: {object: @vendors}
   end
+
+  def create
+    # POST api/1.0/vendor
+    @vendor = Vendor.create!(params[:vendor])
+    render 'shared/vendor/show'
+  end
+
 end
--- lib/report_data.rb
+++ lib/report_data.rb
@@ -7,16 +7,17 @@ class ReportData
   end
 
   def index
-    payment_histories = Transaction.where("status = 1 AND created_at >= ? AND created_at < ?", @from, @to).map do |ph|
-      ph.payment_info
-    end
+    payment_histories = Transaction.where("status = 1 AND created_at >= ? AND created_at < ?", @from, @to).map { |ph| ph.payment_info }
+    [ payment_histories ]
+  end
+
+  def index_with_vendor_id
+    payment_histories = Transaction.where("status = 1 AND created_at >= ? AND created_at < ?", @from, @to).map { |ph| ph.payment_info += ';' + ph.vendor_id.to_s } 
     [ payment_histories ]
   end
 
   def self.index_by_vendor(vendor_id, month)
-    payment_histories = Transaction.where("status = 1 AND extract(month from created_at) = ? AND vendor_id = ?", month.to_i, vendor_id.to_i).map do |ph|
-      ph.payment_info
-    end
+    payment_histories = Transaction.where("status = 1 AND extract(month from created_at) = ? AND vendor_id = ?", month.to_i, vendor_id.to_i).map { |ph| ph.payment_info }
     [ payment_histories ]
   end
 end
