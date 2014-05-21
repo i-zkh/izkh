09297f1d63f3d8e1a3072c5c8afc554b52739510
ivannasya <vagrant@vagrant.(none)>
2014-05-21 13:26:51 +0000

    add vendors

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -39,6 +39,6 @@ class VendorsController < ApplicationController
   end
 
   def vendor_params
-    request.get? ? {} : params.require(:vendor).permit(:title, :service_type_id, :commission)
+    request.get? ? {} : params.require(:vendor).permit(:title, :service_type_id, :commission, :commission_yandex, :commission_ya_card)
   end
 end
