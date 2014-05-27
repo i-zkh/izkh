eb840c03cb7505fe20d1d5a7c526d3024b628e5b
ivannasya <vagrant@vagrant.(none)>
2014-05-27 12:13:12 +0000

    delta

---
+++ app/workers/delta_payment_worker.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+class GtPaymentWorker
+  include Sidekiq::Worker
+  sidekiq_options :retry => false
+
+  def perform(key)
+    response = HTTParty.post( "http://cabinet.izkh.ru/delta_success_pay",
+          :body => {:key => key}.to_json,
+          :headers => {'Content-Type' => 'application/json'})
+  end
+end
