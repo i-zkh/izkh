c93d509e057d625973d4ed206575a6d7c1d7c1d0
ivannasya <vagrant@vagrant.(none)>
2014-05-27 11:23:38 +0000

    service fail

--- app/controllers/delta_payment_controller.rb
+++ app/controllers/delta_payment_controller.rb
@@ -3,14 +3,11 @@ class DeltaPaymentController < ApplicationController
   skip_before_filter :require_current_user
 
   def pay
-    # response = HTTParty.get( "http://cabinet.izkh.ru/delta_payment?key=#{params[:key]}").parsed_response["payment"]
-    # response.each do |r| 
-      # @installation_payment = r['installation_payment']
-      # @service_payment = r['service_payment'] 
-      # @amount = @installation_payment + @service_payment
-    # end
-      @installation_payment = 200
-      @service_payment = 100
+    response = HTTParty.get( "http://cabinet.izkh.ru/delta_payment?key=#{params[:key]}").parsed_response["payment"]
+    response.each do |r| 
+      @installation_payment = r['installation_payment']
+      @service_payment = r['service_payment'] 
       @amount = @installation_payment + @service_payment
+    end
   end
 end
