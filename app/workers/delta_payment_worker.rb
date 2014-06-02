b7cd0703b51a44f124233b157534097de3ab2468
ivannasya <vagrant@vagrant.(none)>
2014-06-02 13:04:56 +0000

    delta

--- app/workers/delta_payment_worker.rb
+++ app/workers/delta_payment_worker.rb
@@ -7,5 +7,21 @@ class GtPaymentWorker
     response = HTTParty.post( "http://cabinet.izkh.ru/delta_success_pay",
           :body => {:key => key}.to_json,
           :headers => {'Content-Type' => 'application/json'})
+
+    mandrill = Mandrill::API.new 'NToYNXQZRClqYQkDai6ujg'
+
+    message = {  
+      :subject=> "АйЖКХ",  
+      :from_name=> "АйЖКХ",  
+      :text=>"Оплата услуг компании DELTA",  
+      :to=>[{
+          :email=> iva.anastya@gmail.com,
+      }],  
+      :html=>
+        "<html><h3>Заявка на оплату услуг компании DELTA</h3>
+        </html>",
+      :from_email=>"out@izkh.ru"
+    } 
+    mandrill.messages.send message
   end
 end
