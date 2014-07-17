f681dac7b016e9b1797b4f82a48d39b6ad284964
ivannasya <vagrant@vagrant.(none)>
2014-07-17 11:28:12 +0000

    index user feedback

--- app/controllers/user_feedbacks_controller.rb
+++ app/controllers/user_feedbacks_controller.rb
@@ -1,17 +1,21 @@
 # encoding: utf-8
 class UserFeedbacksController < ApplicationController
 
-  def index
+  def new
     @show = UserFeedback.where(user_id: current_user.id) == [] ? true : false
   end
 
   def create
     @feedback = UserFeedback.new(user_feedback_params.merge!(user_id: current_user.id))
     if @feedback.save
-      render js: "$('#feedback_error').empty();$('#feedback_topic').val('');$('textarea').val('');", status: :ok
+      render js: "$('#feedback_error').empty();$('#feedback_error').append('Ваше сообщение отправлено, мы ответим Вам в самое ближайшее время!');$('#feedback_topic').val('');$('textarea').val('');", status: :ok
     else
       render js: "$('#feedback_error').empty();$('#feedback_error').append('Необходимо заполнить оба поля')"
     end
+  end
+
+  def index
+    render json: UserFeedback.all
   end
 
   protected
